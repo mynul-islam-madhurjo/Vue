108a9527312fa561277a6f997e67e91fd22f96c6
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2023-02-16 17:37:32 +0600

    js array mapping numeric

--- blog/src/components/Axios.vue
+++ blog/src/components/Axios.vue
@@ -1,10 +1,19 @@
 <template>
 
 <div id="app">
-    <div v-for="post in posts" :key="post.id">
+    <!-- <div v-for="post in posts" :key="post.id">
         <hr>
     <h3>#{{post.id}} {{post.title}}</h3>
-    <p>{{ post.body }}</p>
+    <p>post.body</p>
+    </div> -->
+
+    <div>
+        <ul>
+            <li v-for="(value, index) in filtered" :key="index">
+                {{ value }}
+            </li>
+        </ul>
+
     </div>
 
 
@@ -22,7 +31,8 @@ export default {
     name: "Axios",
     data(){
         return{
-            posts: []
+            posts: [],
+            array1: [1,2,3,4,5]
         }
     },
     methods:{
@@ -37,8 +47,17 @@ export default {
         .catch(error => {
             console.log(error)
         })
-    }
+    },
+    computed: {
 
+    filtered() {
+        return this.array1.map(x => {
+           return x=x+1
+        });
+
+  }
+
+}
 }
 
 
--- blog/src/main.js
+++ blog/src/main.js
@@ -2,3 +2,25 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+//Global filter function 
+
+Vue.filter('snippet', val=>{
+    if(!val || typeof(val) != 'string') return ''
+    val = val.slice(0, 50)
+    console.log(val)
+    return val
+})
+
+// app.js
+const app = createApp(App)
+
+app.config.globalProperties.$filters = {
+  currencyUSD(value) {
+    return '$' + value
+  }
+}
+
+new Vue ({
+    render: h => h(App)
+}).$mount('#app')
