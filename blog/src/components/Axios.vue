100641047bca868efa4620959f9d3b70029658ee
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2023-02-16 11:32:16 +0600

    minor changes

--- blog/src/components/Axios.vue
+++ blog/src/components/Axios.vue
@@ -2,7 +2,8 @@
 
 <div id="app">
     <div v-for="post in posts" :key="post.id">
-    <h3>{{post.title}}</h3>
+        <hr>
+    <h3>#{{post.id}} {{post.title}}</h3>
     <p>{{ post.body }}</p>
     </div>
 
